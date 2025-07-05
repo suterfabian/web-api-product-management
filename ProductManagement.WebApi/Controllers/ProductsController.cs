b77bf3681308a74c052571c275f63e944cf86328
Fabian Suter <fabian.suter@protonx.ch>
2025-07-05 14:39:09 +0200

    Adjust controller

--- ProductManagement.WebApi/Controllers/ProductsController.cs
+++ ProductManagement.WebApi/Controllers/ProductsController.cs
@@ -9,9 +9,9 @@ namespace ProductManagement.WebApi.Controllers;
 public class ProductsController : ControllerBase
 {
     private readonly ILogger<ProductsController> _logger;
-    private readonly CreateProductHandler _createHandler;
-    private readonly GetProductHandler _getHandler;
-    private readonly GetAllProductsHandler _getAllHandler;
+    private readonly CreateProductHandler _createProductHandler;
+    private readonly GetProductHandler _getProductHandler;
+    private readonly GetAllProductsHandler _getAllProductsHandler;
 
     public ProductsController(
         ILogger<ProductsController> logger,
@@ -20,29 +20,29 @@ public class ProductsController : ControllerBase
         GetAllProductsHandler getAllHandler)
     {
         _logger = logger;
-        _createHandler = createHandler;
-        _getHandler = getHandler;
-        _getAllHandler = getAllHandler;
+        _createProductHandler = createHandler;
+        _getProductHandler = getHandler;
+        _getAllProductsHandler = getAllHandler;
     }
 
     [HttpPost]
     public async Task<ActionResult<Guid>> Create([FromBody] CreateProductCommand command)
     {
-        var id = await _createHandler.HandleAsync(command);
+        var id = await _createProductHandler.HandleAsync(command);
         return Ok(id);
     }
 
     [HttpGet("{id}")]
     public async Task<ActionResult<Product>> GetById(Guid id)
     {
-        var product = await _getHandler.HandleAsync(new GetProductQuery(id));
+        var product = await _getProductHandler.HandleAsync(new GetProductQuery(id));
         return product is null ? NotFound() : Ok(product);
     }
 
     [HttpGet]
     public async Task<ActionResult<List<Product>>> GetAll()
     {
-        var products = await _getAllHandler.HandleAsync();
+        var products = await _getAllProductsHandler.HandleAsync();
         return Ok(products);
     }
 }
