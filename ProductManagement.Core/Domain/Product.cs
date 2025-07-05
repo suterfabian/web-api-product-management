ccf846391fe1d112a9a7777e7cbed14c06b5b24f
Fabian Suter <fabian.suter@protonx.ch>
2025-07-05 13:00:26 +0200

    Init

---
+++ ProductManagement.Core/Domain/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace ProductManagement.Core.Domain;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Price { get; set; }
+}
---
+++ ProductManagement.WebApi/Controllers/ProductsController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductManagement.Core.Domain;
+
+namespace ProductManagement.WebApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly ILogger<ProductsController> _logger;
+
+    public ProductsController(ILogger<ProductsController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetProducts")]
+    public IEnumerable<Product> Get()
+    {
+        return Enumerable.Range(1, 5).Select(index => new Product
+        {
+            Id = index,
+            Name = $"Product-{index}",
+            Price = Random.Shared.Next(9000)
+        })
+        .ToArray();
+    }
+}
